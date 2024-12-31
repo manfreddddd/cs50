53bb8af7d6eb56f05bb2f23a776ece72231bf526
Manfred Hau <manfredhaucj357@gmail.com>
2024-12-31 11:06:30 +0800

    week1 qn1

---
+++ week1/deep.py
@@ -0,0 +1,8 @@
+def deep():
+    user_input=input("What is the Answer to the Great Question of Life, the Universe and Everything?:  ")
+    if user_input == "42" or user_input == "forty-two" or user_input == "forty two":
+        print("Yes")
+    else:
+        print("No")
+
+deep()
